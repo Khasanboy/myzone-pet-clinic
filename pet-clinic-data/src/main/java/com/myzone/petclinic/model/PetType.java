4c62f7635fd544565e794486cc1b4142b8508ecf
Khasanboy <akbarov8808@gmail.com>
2019-09-02 22:35:15 +0300

    All POJOs are converted to JPA Entities

--- pet-clinic-data/src/main/java/com/myzone/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/myzone/petclinic/model/PetType.java
@@ -3,10 +3,17 @@ package com.myzone.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "pet_types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
 }
